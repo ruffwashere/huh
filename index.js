a402281b16447a4fa61ceacc135779dc82792624
ruffwashere <ruffwashere@gmail.com>
2020-05-02 16:25:22 +0300

    I guess i added react

--- index.js
+++ index.js
@@ -20,6 +20,32 @@ choosePic = () => {
 mainPage = () => {
     mainDiv.style.opacity = '1';
 }
+
+// const changeTheme = (theme) => {
+//     let color = theme == 'light' ? 'white' : 'black';
+
+//     document.getElementById('nameL').style.color = color;
+//     document.getElementById('developer').style.color = color;
+//     document.getElementById(`${theme}Button`).classList.add('hide');
+//     document.getElementById(`${theme}Button`).classList.remove('hide');
+// }
+
+const mode = {
+    lightMode: () => {
+        mainDiv.style.backgroundColor = 'white',
+        document.getElementById('nameL').style.color = 'black';
+        document.getElementById('developer').style.color = 'black';
+        document.getElementById('lightButton').classList.add('hide');
+        document.getElementById('darkButton').classList.remove('hide');
+    },
+    darkMode: () => {
+        mainDiv.style.backgroundColor = 'black',
+        document.getElementById('nameL').style.color = 'white';
+        document.getElementById('developer').style.color = 'white';
+        document.getElementById('darkButton').classList.add('hide');
+        document.getElementById('lightButton').classList.remove('hide');
+    }
+}
 // dloads the page after 2 seconds
 loadedPage = () => {
     setTimeout(() => {
